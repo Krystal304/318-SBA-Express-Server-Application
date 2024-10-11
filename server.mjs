cd5c23e5bc14f06aeaed17288077c3b58f860485
Krystal304 <krystalwilliams864@gmail.com>
2024-10-11 16:16:15 -0400

    initial

---
+++ server.mjs
@@ -0,0 +1,63 @@
+
+//imports
+import express from 'express';
+
+
+//instance of express and invoke
+
+const app = express ()
+ 
+
+//view engine
+app.set('view engine', 'ejs')
+
+//pass middleware
+app.use (logger)
+
+
+//port
+
+let PORT = 3000
+
+
+//middleware
+
+
+function logger(req, res, next){
+    console.log('log')
+    next()
+}
+
+//routes CRUD
+
+app.get('/', (req, res)=>{
+    console.log('Home Page')
+    res.render('index')
+})
+
+app.get('/users/:id/', (req, res)=>{
+    const id = req.params.id;
+    res.json ({
+        id: id,
+        name: 'donald duck'
+
+    })
+})
+
+app.post('/users', (req, res)=>{
+    res.json({id:1, name: 'Krissy Ball', sport: 'swim', age: 15});
+});
+
+app.put('/users', (req, res)=>{
+    res.json({id:1, name: 'Krissy Ball', sport: 'tennis', age: 16});
+})  
+
+app.delete('/users', (req, res)=>{
+    res.json();
+})
+
+//listen 
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
